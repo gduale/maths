10fd6e07dc1005af8195ba323a56e3bc0dcb937f
gduale <guillaume.duale+gduale@gmail.com>
2026-09-23 19:32:51 +0200

    debug off

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-epx=g&06-@r88hbfxnz%_$b7se1g-+r5c()p+0t7zk(2i&c!&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'testserver']
 
